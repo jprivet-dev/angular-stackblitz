010e6cea6e1cd03e38d9400363e6fd09989cece9
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-02-20 11:51:23 +0100

    feat(generate-root): avoid error in stackblitz

--- scripts/generate-root.sh
+++ scripts/generate-root.sh
@@ -8,6 +8,7 @@ echo "# Generate a new Angular app (in root) #"
 echo "########################################"
 
 echo "> Remove the old Angular app"
+
 rm .editorconfig
 rm .gitignore
 rm -rf .vscode
@@ -19,6 +20,10 @@ rm tsconfig*.json
 rm -rf node_modules
 
 echo "> Create an Angular app"
+
 ng new app --directory ./ --skip-git true --routing --style scss --defaults --strict
+
+line="import 'zone.js/dist/zone'; // Avoid in StackBlitz \"Error: NG0908: In this configuration Angular requires Zone.js\""
+sed  -i "1i ${line}" ./src/main.ts
 
 echo "> Builds and serves your application with \"$ ng serve\""
