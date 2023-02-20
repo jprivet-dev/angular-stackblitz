74aba37ce2458e45e927dc924b68a98c9147593e
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-02-20 15:11:05 +0100

    feat(generate-root): update

--- scripts/generate-root.sh
+++ scripts/generate-root.sh
@@ -8,7 +8,6 @@ echo "# Generate a new Angular app (in root) #"
 echo "########################################"
 
 echo "> Remove the old Angular app"
-
 rm .editorconfig
 rm .gitignore
 rm -rf .vscode
@@ -20,9 +19,9 @@ rm tsconfig*.json
 rm -rf node_modules
 
 echo "> Create an Angular app"
-
 ng new app --directory ./ --skip-git true --routing --style scss --defaults --strict
 
+echo "> Avoid error in StackBlitz"
 line="import 'zone.js/dist/zone'; // Avoid error in StackBlitz"
 sed  -i "1i ${line}" ./src/main.ts
 
